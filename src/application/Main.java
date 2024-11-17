90b1630e6d0b9edf1afdfa6695c47a72450de83a
coder-gaia <alexandregaia911@gmail.com>
2024-11-17 20:13:09 -0300

    Implementing search by department

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,7 +3,11 @@ package application;
 
 import model.dao.DAOFactory;
 import model.dao.SellerDAO;
+import model.entities.Department;
 import model.entities.Seller;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,5 +16,13 @@ public class Main {
         SellerDAO sellerDAO = DAOFactory.createSellerDAO();
         Seller seller = sellerDAO.findById(3);
         System.out.println(seller);
+
+        System.out.println("\n === TEST nº01: seller findByDepartment ===");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDAO.findByDepartment(department);
+
+        for(Seller obj : list){
+            System.out.println(obj);
+        }
     }
 }
